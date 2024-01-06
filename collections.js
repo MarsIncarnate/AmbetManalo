962b46cbc81c20cc2de985b4606c90aef61d1554
MarsIncarnate <marvellousadeogun@gmail.com>
2024-01-06 23:22:18 +0100

    add product item code and update collections with code to display all the collections before filtering

--- collections.js
+++ collections.js
@@ -4,33 +4,38 @@ import wixLocationFrontend from 'wix-location-frontend';
 $w.onReady(function () {
 
 	function findUpdate(catName, index, imageId, titleId, yearId, descId, boxId) {
-		wixData.query("Portfolio1")
-			.eq('category', catName)
-			.find()
+		let query = wixData.query("Portfolio1");
+
+		if (catName !== "all") {
+			query = query.eq('category', catName);
+		}
+
+		query.find()
 			.then((results) => {
-				if(results.items.length > 0) {
+				if (results.items.length > 0) {
 					const item = results.items[index];
-					$w(imageId).src = item.image
-					$w(titleId).text = item.title
-					$w(yearId).text = item.year
-					$w(descId).text = item.previewText
-					$w(imageId).show()
-					$w(boxId).show()
+					console.log(item);
+					$w(imageId).src = item.image;
+					$w(titleId).text = item.title;
+					$w(yearId).text = item.year;
+					$w(descId).text = item.previewText;
+					$w(imageId).show();
+					$w(boxId).show();
 					const formattedTitle = item.title.replace(/\s+/g, '-').toLowerCase();
-                    const itemPagePath = `/objects/${formattedTitle}`;
+					const itemPagePath = `/objects/${formattedTitle}`;
 					$w(imageId).onClick(() => {
-						console.log(itemPagePath)
-                        // Navigate to the corresponding item page
-                        wixLocationFrontend.to(itemPagePath);
-                    });
+						console.log(itemPagePath);
+						// Navigate to the corresponding item page
+						wixLocationFrontend.to(itemPagePath);
+					});
 				} else {
-				// handle case where no matching items found
+					// handle case where no matching items found
 				}
 			})
 			.catch((err) => {
 				console.log(err);
-			});	
-	}
+			});
+}
 
 	
 	let itemsArr = [
@@ -41,10 +46,14 @@ $w.onReady(function () {
 	{ imageId: '#imageX17', titleId: '#text63', yearId: '#text62', descId: '#text61', boxId: '#box57' },
 	{ imageId: '#imageX18', titleId: '#text66', yearId: '#text65', descId: '#text64', boxId: '#box59' },
 	{ imageId: '#imageX19', titleId: '#text69', yearId: '#text68', descId: '#text67', boxId: '#box61' },
+	{ imageId: '#imageX25', titleId: '#text103', yearId: '#text102', descId: '#text101', boxId: '#box74' },
+	{ imageId: '#imageX23', titleId: '#text97', yearId: '#text96', descId: '#text95', boxId: '#box68' },
+	{ imageId: '#imageX24', titleId: '#text100', yearId: '#text99', descId: '#text98', boxId: '#box71' },
+	{ imageId: '#imageX26', titleId: '#text106', yearId: '#text105', descId: '#text104', boxId: '#box77' }
     // Add more items as needed
 ];
 
-let dropdownValue = 'Furniture'
+let dropdownValue = 'all'
 
 for (let i = 0; i < itemsArr.length; i++) {
     findUpdate(dropdownValue, i, itemsArr[i].imageId, itemsArr[i].titleId, itemsArr[i].yearId, itemsArr[i].descId, itemsArr[i].boxId);
@@ -58,6 +67,14 @@ $w('#dropdown1').onChange((event) => {
 			findUpdate(newValue, i, itemsArr[i].imageId, itemsArr[i].titleId, itemsArr[i].yearId, itemsArr[i].descId, itemsArr[i].boxId);
 		}
     });
+	$w('#box41').onViewportEnter(() => {
+        $w('#section17').hide("fade", { duration: 200 })
+		$w('#section18').hide("fade", { duration: 200 })
+    })
+    $w('#box41').onViewportLeave(() => {
+		$w('#section17').show("fade", { duration: 200 })
+        $w('#section18').show("fade", { duration: 200 })
+    })
 
 	
 });
---
+++ product(item).js
@@ -0,0 +1,57 @@
+import wixData from 'wix-data';
+import wixLocation from 'wix-location';
+
+$w.onReady(async function () {
+    $w('#box41').onViewportEnter(() => {
+        $w('#section17').hide("fade", { duration: 200 })
+    })
+    $w('#box41').onViewportLeave(() => {
+        $w('#section17').show("fade", { duration: 200 })
+    })
+    // Base name of product pages
+    const basePageName = 'products';
+
+    // Get the current page's name
+    const currentPageName = wixLocation.path[0];
+    console.log(currentPageName)
+
+    // Fetch all product slugs from the "Products" collection
+    const productSlugs = await getProductsSlugs();
+
+    // Find the index of the current page in the array of product slugs
+    const currentPageIndex = productSlugs.indexOf(currentPageName.replace(basePageName, ''));
+
+    // Calculate the next and previous page indexes
+    const nextPageIndex = currentPageIndex + 1;
+    const prevPageIndex = currentPageIndex - 1;
+
+    // Set the links for next and previous pages
+    const nextPageLink = nextPageIndex < productSlugs.length ? `/${basePageName}/${productSlugs[nextPageIndex]}` : null;
+    const prevPageLink = prevPageIndex >= 0 ? `/${basePageName}/${productSlugs[prevPageIndex]}` : null;
+    console.log(nextPageLink, prevPageLink)
+
+    // Hide or show the next and previous buttons based on availability
+    if (nextPageLink) {
+        $w('#button2').link = nextPageLink;
+        $w('#button2').show();
+    } else {
+        $w('#button2').hide();
+    }
+
+    if (prevPageLink) {
+        $w('#previousBtn').link = prevPageLink;
+        $w('#previousBtn').show();
+    } else {
+        $w('#previousBtn').hide();
+    }
+});
+
+async function getProductsSlugs() {
+    // Query the "Products" collection to get all product slugs
+    const results = await wixData.query('Stores/Products')
+        .find();
+
+    // Extract and return an array of product slugs
+    console.log(results)
+    return results.items.map(item => item.slug);
+}
